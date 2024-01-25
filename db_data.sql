50d3bcf3060db996500b7fbf3f52df0cf88a9f40
limdongsun0814 <118763659+limdongsun0814@users.noreply.github.com>
2024-01-25 22:36:18 +0900

    Update db_data.sql

--- db_data.sql
+++ db_data.sql
@@ -1,16 +1,4 @@
--- --------------------------------------------------------
--- 호스트:                          127.0.0.1
--- 서버 버전:                        11.2.2-MariaDB-1:11.2.2+maria~ubu2204 - mariadb.org binary distribution
--- 서버 OS:                        debian-linux-gnu
--- HeidiSQL 버전:                  11.3.0.6295
--- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
 CREATE DATABASE IF NOT EXISTS `testdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
@@ -45,14 +33,6 @@ CREATE TABLE IF NOT EXISTS `test2` (
   KEY `title` (`title`(768))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- 테이블 데이터 testdb.test2:~2 rows (대략적) 내보내기
-/*!40000 ALTER TABLE `test2` DISABLE KEYS */;
 INSERT INTO `test2` (`title`, `name`) VALUES
 	('안뇽', '안녕'),
 	('123', '11');
-/*!40000 ALTER TABLE `test2` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
