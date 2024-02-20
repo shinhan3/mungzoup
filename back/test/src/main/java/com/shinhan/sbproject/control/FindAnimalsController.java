a7dcfd97f6dca18908544abfecaec47ec5ee5336
limdongsun0814 <asme12@naver.com>
2024-02-20 17:19:33 +0900

    2024-02-20

--- back/test/src/main/java/com/shinhan/sbproject/control/FindAnimalsController.java
+++ back/test/src/main/java/com/shinhan/sbproject/control/FindAnimalsController.java
@@ -1,6 +1,8 @@
 package com.shinhan.sbproject.control;
 
 import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -77,7 +79,15 @@ public class FindAnimalsController {
 
          int w = imageRgb.getWidth();
 		 int h = imageRgb.getHeight();
-         
+
+		 
+          File convFile = new File("C:\\Users\\User\\Documents\\카카오톡 받은 파일\\도착위치.png");
+         convFile.createNewFile();
+         FileOutputStream fos = new FileOutputStream(convFile);
+        //  fos.write(imageFile.getBytes());
+        //  fos.close();
+		
+		ImageIO.write(imageRgb, "jpg", fos);
 
          int[] dataBuffInt = imageRgb.getRGB(0, 0, 32, 32, null, 0, 32); 
 		System.out.println(dataBuffInt.length);
@@ -85,10 +95,12 @@ public class FindAnimalsController {
          //[개수][32][32][rgb] 형태로 넣어줌
 		float[][][][] input= rgbImageToArray(32,32,dataBuffInt);
 		System.out.println(input[0].length);
+		System.out.println(Arrays.deepToString(input));
 		// String filePath = "C:\\Users\\wldnj\\git\\project3-1\\back\\test\\src\\main\\java\\com\\shinhan\\sbproject\\control\\data\\test.csv";
-			
-
-         try(SavedModelBundle b = SavedModelBundle.load(modelPath+"/abcd", "serve")){ 
+		
+		
+		System.out.println(modelPath+"/abcde");
+         try(SavedModelBundle b = SavedModelBundle.load(modelPath+"/abcde", "serve")){ 
 				//default가 서브, 도커에 생긴 abcd를 컨트롤러 아래로 복붙
 				
 				System.out.println("tttt");
@@ -107,9 +119,17 @@ public class FindAnimalsController {
 						.copyTo(new float[1][16]); //class 2개, 입력 1개라 다음과 같이 선언함. [[],[]]
 				
 				//print out the result
-            for(int i=0; i<16; i++)
+				float max = 0;
+				int argmax = 0;
+            for(int i=0; i<16; i++){
                   System.out.println("결과: " + y[0][i]); //16classification : 출력
-				// for(int i=0; i<y.length;i++)
+				if(max< y[0][i]){
+					max =  y[0][i];
+					argmax = i;
+				}
+			}
+			System.out.println(argmax+1);
+				  // for(int i=0; i<y.length;i++)
 				// 	System.out.println(y[i][1]+"결과: "+y[i][0]); //2classification : [0][1] or [1][0]으로 출력
 					// [[0],[1]]: others, [[1],[0]]: robot
 					// [0][1]=1 => others
@@ -122,27 +142,40 @@ public class FindAnimalsController {
       return "entity";
    }
 
-    public BufferedImage convertBytesToRGBImage(byte[] bytes) throws IOException {
-		// Read the byte array into a BufferedImage
-		ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
-		BufferedImage bufferedImage = ImageIO.read(bis);
+   public BufferedImage convertBytesToRGBImage(byte[] bytes) throws IOException {
+	// Read the byte array into a BufferedImage
+	ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+	BufferedImage bufferedImage = ImageIO.read(bis);
 
-		// Ensure that the image is in the correct format (RGB)
-		BufferedImage convertedImage = new BufferedImage(bufferedImage.getWidth(), bufferedImage.getHeight(), BufferedImage.TYPE_INT_RGB);
-		convertedImage.createGraphics().drawImage(bufferedImage, 0, 0, null);
+	// Resize the image to 32x32
+	Image scaledImage = bufferedImage.getScaledInstance(32, 32, Image.SCALE_SMOOTH);
 
-		return convertedImage;
+	// Convert the scaled image back to a BufferedImage
+	BufferedImage resizedImage = new BufferedImage(32, 32, BufferedImage.TYPE_INT_RGB);
+	Graphics2D g2d = resizedImage.createGraphics();
+	g2d.drawImage(scaledImage, 0, 0, null);
+	g2d.dispose();
 
-    }
+	return resizedImage;
+}
 
     public float[][][][] rgbImageToArray(int w, int h, int[] dataBuffInt){
 		float[][][][] result = new float[1][w][h][3];
-		for(int i=0; i<w;i++){
-			for(int j=0; j<h;j++){
-				Color color = new Color(dataBuffInt[i*j],false);
-				result[0][i][j][0]=color.getRed();
-				result[0][i][j][1]=color.getGreen();
-				result[0][i][j][2]=color.getBlue();
+		for(int i=0; i<h;i++){
+			for(int j=0; j<w;j++){
+				// Color color = new Color(dataBuffInt[i*j],false);
+				// result[0][i][j][0]=color.getRed();
+				// result[0][i][j][1]=color.getGreen();
+				// result[0][i][j][2]=color.getBlue();
+
+				int pixel = dataBuffInt[i * w + j];
+				int red = (pixel >> 16) & 0xFF;
+				int green = (pixel >> 8) & 0xFF;
+				int blue = pixel & 0xFF;
+				result[0][i][j][0] = blue;
+				result[0][i][j][1] = green;
+				result[0][i][j][2] = red;
+
 			}
 		}
 		return result;
